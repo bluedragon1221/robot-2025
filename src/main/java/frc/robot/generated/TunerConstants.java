76bbb00a6b31539be895979332d56feb9e8a47fa
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-01-16 16:26:51 -0600

    add test paths; TunerConstants; bindings.md

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -55,6 +55,7 @@ public class TunerConstants {
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
+    // TODO: tune current limits to avoid brownouts
     private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
