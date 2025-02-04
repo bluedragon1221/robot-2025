30e388ebdff48e5188fe02d130a0769a4dad9e7f
Driver Station <programming.frc.8114@gmail.com>
2025-02-04 16:24:24 -0600

    more descriptive constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import edu.wpi.first.units.measure.Angle;
+
 public final class Constants {
     public static final class AlgaeArmConstants {
         public static final int pivotMotorID = 0;
@@ -11,7 +14,7 @@ public final class Constants {
         public static final int pivotMotorAcceleration = 160;
         public static final int pivotMotorCruiseVelocity = 80;
 
-        public static final int pivotMotorTolerance = 1; // IN DEGREES
+        public static final Angle pivotMotorTolerance = Degrees.of(1.0);
 
         public static final int pivotEncoderID = 0;
         public static final int gripperMotorID = 0;
@@ -21,7 +24,7 @@ public final class Constants {
         public static final int leftMotorID = 0;
         public static final int rightMotorID = 0;
 
-        public static final int motorAcceleration = 160;
+        public static final int motorMaxAcceleration = 160;
         public static final int motorCruiseVelocity = 80;
 
         public static final int heightSensorID = 0;
