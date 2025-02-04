4fc899ed051f140aae902ce90c4974a751d2cd78
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-02-04 16:30:10 -0600

    work on constants (not all zeros anymore)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,30 +4,32 @@ import static edu.wpi.first.units.Units.Degrees;
 import edu.wpi.first.units.measure.Angle;
 
 public final class Constants {
-    public static final class AlgaeArmConstants {
-        public static final int pivotMotorID = 0;
-        public static final int gripperMotorID = 0;
+    public static final class ElevatorConstants {
+        public static final int leftMotorID = 20;
+        public static final int rightMotorID = 21;
+
+        public static final int motorAcceleration = 160;
+        public static final int motorCruiseVelocity = 80;
+
+        public static final int heightSensorID = 22;
+        public static final int bottomSensorGPIO = 0;
     }
 
     public static final class CoralArmConstants {
-        public static final int pivotMotorID = 0;
+        public static final int pivotMotorID = 31;
+        public static final int pivotEncoderID = 32;
         public static final int pivotMotorAcceleration = 160;
         public static final int pivotMotorCruiseVelocity = 80;
 
         public static final Angle pivotMotorTolerance = Degrees.of(1.0);
 
-        public static final int pivotEncoderID = 0;
-        public static final int gripperMotorID = 0;
+        public static final int gripperMotorID = 33;
     }
 
-    public static final class ElevatorConstants {
-        public static final int leftMotorID = 0;
-        public static final int rightMotorID = 0;
-
-        public static final int motorMaxAcceleration = 160;
-        public static final int motorCruiseVelocity = 80;
-
-        public static final int heightSensorID = 0;
-        public static final int bottomSensorGPIO = 0;
+    public static final class AlgaeArmConstants {
+        public static final int pivotMotorID = 35;
+        public static final int gripperMotorID = 36;
     }
+
+    public static final class ClimberConstants {}
 }
