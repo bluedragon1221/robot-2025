c2bbee8d8c4e825de0f658c2da5e942584c45bfe
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-03-07 09:40:29 -0600

    lots of changes to alignReef code

--- src/main/java/frc/robot/commands/AlignReef.java
+++ src/main/java/frc/robot/commands/AlignReef.java
@@ -1,4 +1,5 @@
 package frc.robot.commands;
+
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -8,27 +9,23 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class AlignReef extends Command {
-    private final static TrapezoidProfile.Constraints x_constraints = new TrapezoidProfile.Constraints(3, 2);
-    private final static TrapezoidProfile.Constraints y_constraints = new TrapezoidProfile.Constraints(3, 2);
-    private final static TrapezoidProfile.Constraints theta_constraints = new TrapezoidProfile.Constraints(8, 8);
+    private static final TrapezoidProfile.Constraints x_constraints = new TrapezoidProfile.Constraints(3, 2);
+    private static final TrapezoidProfile.Constraints y_constraints = new TrapezoidProfile.Constraints(3, 2);
+    private static final TrapezoidProfile.Constraints theta_constraints = new TrapezoidProfile.Constraints(8, 8);
 
     private final ProfiledPIDController controller_x;
     private final ProfiledPIDController controller_y;
     private final ProfiledPIDController controller_theta;
-
-    private static final double position_tolerance = 0.5;
-    private static final double angle_tolerance = 1;
 
     private final Pose2d goal_pose;
 
     private final SwerveRequest.FieldCentric drive_request;
     private final CommandSwerveDrivetrain drivetrain;
 
-    public AlignReef(CommandSwerveDrivetrain drivetrain, SwerveRequest.FieldCentric drive_request, Pose2d goal_pose) {
-        // this feels like python...
-        this.drive_request = drive_request;
-        this.drivetrain = drivetrain;
-        this.goal_pose = goal_pose;
+    public AlignReef(CommandSwerveDrivetrain driveTrain, SwerveRequest.FieldCentric driveRequest, Pose2d goalPose) {
+        drive_request = driveRequest;
+        drivetrain = driveTrain;
+        goal_pose = goalPose;
 
         controller_x = new ProfiledPIDController(3, 0, 0, x_constraints);
         controller_y = new ProfiledPIDController(3, 0, 0, y_constraints);
@@ -36,9 +33,12 @@ public class AlignReef extends Command {
     }
 
     private static boolean posesNear(Pose2d pose1, Pose2d pose2) {
+        double position_tolerance = 0.5;
+        double angle_tolerance = 1.0;
+
         return (Math.abs(pose1.getX() - pose2.getX()) < position_tolerance) &&
                (Math.abs(pose1.getY() - pose2.getY()) < position_tolerance) &&
-               (Math.abs(pose1.getRotation().getDegrees() - pose2.getRotation().getDegrees()) > angle_tolerance);
+               (Math.abs(pose1.getRotation().getDegrees() - pose2.getRotation().getDegrees()) < angle_tolerance);
     }
 
     @Override
@@ -51,10 +51,12 @@ public class AlignReef extends Command {
     @Override
     public void execute() {
         Pose2d curr_pose = drivetrain.getState().Pose;
-        drive_request
-            .withVelocityX(controller_x.calculate(curr_pose.getX(), goal_pose.getX()))
-            .withVelocityY(controller_y.calculate(curr_pose.getY(), goal_pose.getY()))
-            .withRotationalRate(controller_theta.calculate(curr_pose.getRotation().getDegrees(), goal_pose.getRotation().getDegrees()));
+        drivetrain.setControl(
+            drive_request
+                .withVelocityX(controller_x.calculate(curr_pose.getX(), goal_pose.getX()))
+                .withVelocityY(controller_y.calculate(curr_pose.getY(), goal_pose.getY()))
+                .withRotationalRate(controller_theta.calculate(curr_pose.getRotation().getDegrees(), goal_pose.getRotation().getDegrees()))
+        );
     }
 
     @Override
@@ -65,6 +67,11 @@ public class AlignReef extends Command {
 
     @Override
     public void end(boolean interupted) {
-        drivetrain.setControl(drive_request.withVelocityX(0).withVelocityY(0).withRotationalRate(0));
+        drivetrain.setControl(
+            drive_request
+                .withVelocityX(0)
+                .withVelocityY(0)
+                .withRotationalRate(0)
+        );
     }
 }
