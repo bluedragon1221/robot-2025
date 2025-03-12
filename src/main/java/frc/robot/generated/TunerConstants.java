5bca881a7e0ace318bc4f241be5dbb606df163ea
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-03-12 13:46:32 -0500

    wheel radius

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -82,7 +82,7 @@ public class TunerConstants {
 
     private static final double kDriveGearRatio = 6.746031746031747;
     private static final double kSteerGearRatio = 12.8;
-    private static final Distance kWheelRadius = Inches.of(2);
+    private static final Distance kWheelRadius = Inches.of(2.0910);
 
     private static final boolean kInvertLeftSide = false;
     private static final boolean kInvertRightSide = true;
